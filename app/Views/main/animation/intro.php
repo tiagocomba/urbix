15a6900b03bb79de2027ce94b5a76cd7af91c069
ᴄᴏᴍʙᴀ.ᴄᴏᴅᴇ <tiagocomba89@proton.me>
2023-10-05 11:55:12 -0300

    rutas add intros

--- app/Views/main/animation/intro.php
+++ app/Views/main/animation/intro.php
@@ -3,15 +3,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>KeyTech</title>
-    <link rel="stylesheet" href="css/intro.css">
+    <title>catalogo</title>
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/intro.css');?>">
     <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-    <script src="js/pace.js"></script>
+    <script src="<?php echo base_url('assets/js/pace.js');?>"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery-bez@1.0.11/src/jquery.bez.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.2/TweenMax.min.js"></script>
 </head>
 <body>
-        <h1 class="title">KEYTECH</h1>
+        <h1 class="title">urbix</h1>
 
         <div id="preloader">
             <div class="p">LOADING</div>
@@ -26,25 +26,25 @@
         };
 
         Pace.on('done', function() {
-        $('.p').delay(500).animate({top: '30%', opacity: '0'}, 3000, $.bez([0.19,1,0.22,1]));
+    $('.p').delay(500).animate({top: '30%', opacity: '0'}, 1000, $.bez([0.19,1,0.22,1]));
+    $('#preloader').delay(1500).animate({top: '-100%'}, 1000, $.bez([0.19,1,0.22,1]));
 
+    TweenMax.from(".title", 1, {
+        delay: 1.8,
+        y: 10,
+        opacity: 0,
+        ease: Expo.easeInOut
+    });
 
-        $('#preloader').delay(1500).animate({top: '-100%'}, 2000, $.bez([0.19,1,0.22,1]));
-
-        TweenMax.from(".title", 2, {
-             delay: 1.8,
-                  y: 10,
-                  opacity: 0,
-                  ease: Expo.easeInOut
-            })
-            setTimeout(function () {
-    // Genera la URL absoluta utilizando site_url()
-    var absoluteUrl = "<?php echo site_url('inicio'); ?>";
+    setTimeout(function () {
+        // Genera la URL absoluta utilizando site_url()
+        var absoluteUrl = "<?php echo site_url('catalogo'); ?>";
     
-    // Redirige a la URL absoluta
-    window.location.href = absoluteUrl;
-}, 3000);
-       });
+        // Redirige a la URL absoluta
+        window.location.href = absoluteUrl;
+    }, 1000); // Reduje el tiempo de espera a 2 segundos
+});
+
 
       </script>
 
--- app/Views/main/animation/intro_contact.php
+++ app/Views/main/animation/intro_contact.php
@@ -3,8 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Intro urbix</title>
-    <link rel="website icon" type="png" href="<?php echo base_url('assets/img/primary/urbix.png');?>">
+    <title>intro contact</title>
     <link rel="stylesheet" href="assets/css/intro2.css">
  
 </head>
@@ -14,17 +13,17 @@
     <div class="container">
         <div class="text-wrapper">
             <!-- Agrega la clase "text" a cada elemento de texto -->
-            <div class="text text-1">less is more.</div>
-            <div class="text text-2">less is more.</div>
-            <div class="text text-3">less is more.</div>
-            <div class="text text-4">less is more.</div>
-            <div class="text text-5">less is more.</div>
-            <div class="text text-6">less is more.</div>
-            <div class="text text-7">less is more.</div>
-            <div class="text text-8">less is more.</div>
-            <div class="text text-9">less is more.</div>
-            <div class="text text-10">less is more.</div>
-            <div class="text text-11">less is more.</div>
+            <div class="text text-1">Opt for excellence over abundance.</div>
+            <div class="text text-2">Opt for excellence over abundance.</div>
+            <div class="text text-3">Opt for excellence over abundance.</div>
+            <div class="text text-4">Opt for excellence over abundance.</div>
+            <div class="text text-5">Opt for excellence over abundance.</div>
+            <div class="text text-6">Opt for excellence over abundance.</div>
+            <div class="text text-7">Opt for excellence over abundance.</div>
+            <div class="text text-8">Opt for excellence over abundance.</div>
+            <div class="text text-9">Opt for excellence over abundance.</div>
+            <div class="text text-10">Opt for excellence over abundance.</div>
+            <div class="text text-11">Opt for excellence over abundance.</div>
         </div>
     </div>
 
@@ -35,7 +34,7 @@
         // Función para redirigir después de que la animación termine
         function redirectToAnotherView() {
             // Cambia la URL a la que deseas redirigir al usuario
-            var absoluteUrl = "<?php echo site_url('inicio'); ?>";
+            var absoluteUrl = "<?php echo site_url('contact'); ?>";
             window.location.href = absoluteUrl;
         }
 
